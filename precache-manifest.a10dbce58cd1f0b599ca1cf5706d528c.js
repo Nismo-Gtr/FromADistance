733747dd8469d56d7a0f0b397f6ca140ffe7042a
Nismo-Gtr <NCowan3@outlook.com>
2019-07-29 16:23:22 -0500

    Updates

---
+++ precache-manifest.a10dbce58cd1f0b599ca1cf5706d528c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27044b4397a4dda6324dc1de10d1c69b",
+    "url": "/FromADistance/index.html"
+  },
+  {
+    "revision": "c26bcd42ad0437424c55",
+    "url": "/FromADistance/static/css/main.34e36d89.chunk.css"
+  },
+  {
+    "revision": "8277f8c3e485b19ef9b6",
+    "url": "/FromADistance/static/js/2.573d5e5c.chunk.js"
+  },
+  {
+    "revision": "c26bcd42ad0437424c55",
+    "url": "/FromADistance/static/js/main.81a2920d.chunk.js"
+  },
+  {
+    "revision": "847c9e63221cefeff86d",
+    "url": "/FromADistance/static/js/runtime~main.dd8bbf42.js"
+  },
+  {
+    "revision": "29678bf537563c2b12a999e4a272efa2",
+    "url": "/FromADistance/static/media/Logo-1.29678bf5.svg"
+  },
+  {
+    "revision": "bc6c5041d450ee49aca38ad3333e54b2",
+    "url": "/FromADistance/static/media/Logo-2.bc6c5041.svg"
+  },
+  {
+    "revision": "dbfb7d4b5c7ac7386734e9ba3f456502",
+    "url": "/FromADistance/static/media/business.dbfb7d4b.jpg"
+  },
+  {
+    "revision": "661084387c745b233619cf7ef0a7e05a",
+    "url": "/FromADistance/static/media/entertainment.66108438.jpg"
+  },
+  {
+    "revision": "5e4cbc4da7ef883a7387955ac7e87dc5",
+    "url": "/FromADistance/static/media/health-watch.5e4cbc4d.jpg"
+  },
+  {
+    "revision": "6ff8a642e050e85f63533209d4b85707",
+    "url": "/FromADistance/static/media/mountains.6ff8a642.jpg"
+  },
+  {
+    "revision": "7b284636b58b1a7ac8dfeccd83659bf1",
+    "url": "/FromADistance/static/media/science.7b284636.jpg"
+  },
+  {
+    "revision": "7d0b1a38b8c229d8be48ed2a3c2f3a99",
+    "url": "/FromADistance/static/media/sports.7d0b1a38.jpg"
+  },
+  {
+    "revision": "81175f3288ce89d00ca6112f11fae148",
+    "url": "/FromADistance/static/media/tech.81175f32.jpg"
+  }
+]);
