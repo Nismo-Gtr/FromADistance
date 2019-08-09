28e0f403c59ec7ea424beaa34a694455b75682af
Nismo-Gtr <NCowan3@outlook.com>
2019-08-08 22:28:03 -0500

    Updates

---
+++ precache-manifest.353450bed0c0f8bb53a5264fdc1c73fe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7b4d9b7430b738fbc8fa9f5ca5878f",
+    "url": "/FromADistance/index.html"
+  },
+  {
+    "revision": "db02c2781a94e52ddb0e",
+    "url": "/FromADistance/static/css/main.a418aa5f.chunk.css"
+  },
+  {
+    "revision": "823d0938e4b0bfae021d",
+    "url": "/FromADistance/static/js/2.568c3274.chunk.js"
+  },
+  {
+    "revision": "db02c2781a94e52ddb0e",
+    "url": "/FromADistance/static/js/main.de61c497.chunk.js"
+  },
+  {
+    "revision": "847c9e63221cefeff86d",
+    "url": "/FromADistance/static/js/runtime~main.dd8bbf42.js"
+  },
+  {
+    "revision": "29678bf537563c2b12a999e4a272efa2",
+    "url": "/FromADistance/static/media/Logo-1.29678bf5.svg"
+  },
+  {
+    "revision": "bc6c5041d450ee49aca38ad3333e54b2",
+    "url": "/FromADistance/static/media/Logo-2.bc6c5041.svg"
+  },
+  {
+    "revision": "dbfb7d4b5c7ac7386734e9ba3f456502",
+    "url": "/FromADistance/static/media/business.dbfb7d4b.jpg"
+  },
+  {
+    "revision": "661084387c745b233619cf7ef0a7e05a",
+    "url": "/FromADistance/static/media/entertainment.66108438.jpg"
+  },
+  {
+    "revision": "5e4cbc4da7ef883a7387955ac7e87dc5",
+    "url": "/FromADistance/static/media/health-watch.5e4cbc4d.jpg"
+  },
+  {
+    "revision": "6ff8a642e050e85f63533209d4b85707",
+    "url": "/FromADistance/static/media/mountains.6ff8a642.jpg"
+  },
+  {
+    "revision": "7b284636b58b1a7ac8dfeccd83659bf1",
+    "url": "/FromADistance/static/media/science.7b284636.jpg"
+  },
+  {
+    "revision": "7d0b1a38b8c229d8be48ed2a3c2f3a99",
+    "url": "/FromADistance/static/media/sports.7d0b1a38.jpg"
+  },
+  {
+    "revision": "81175f3288ce89d00ca6112f11fae148",
+    "url": "/FromADistance/static/media/tech.81175f32.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.333aefefd41ab03261d5e2302d85c511.js"
+  "/FromADistance/precache-manifest.353450bed0c0f8bb53a5264fdc1c73fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FromADistance/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
